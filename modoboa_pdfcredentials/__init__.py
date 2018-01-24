1cf523cb10d7829780d7271d0939282f68845be3
Andrew Fyfe <andrew@neptune-one.net>
2018-01-24 20:13:23 +0000

    refactor setup.py
    - use setuptools_scm to generate version number, normal releases will
    use the tagged version (ie 1.9.1), versions installed from source will
    append the git commit number to the version number (ie
    1.9.1.dev61+g4584c93)
    
    - use io.open() to read text files to correctly handle utf-8 characters
    
    - use pip to parse requirements.txt
    
    - update classifiers to specify python and django versions supported
    
    - wheel distributions can now be built using `python setup.py bdist_wheel`

--- modoboa_pdfcredentials/__init__.py
+++ modoboa_pdfcredentials/__init__.py
@@ -1,3 +1,16 @@
-__version__ = "1.2.1"
+# -*- coding: utf-8 -*-
+
+"""Generate PDF documents containing user credentials."""
+
+from __future__ import unicode_literals
+
+from pkg_resources import get_distribution, DistributionNotFound
+
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
 
 default_app_config = "modoboa_pdfcredentials.apps.PDFCredentialsConfig"
